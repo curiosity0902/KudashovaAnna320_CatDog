b0f7a061f3102d7c537ba29acbc11a894a9fd606
Анна Кудашова <annakudashova0902@gmail.com>
2023-10-26 09:31:23 +0300

    добавила новые страницы

--- Kudashova320/MainWindow.xaml.cs
+++ Kudashova320/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Kudashova320.Pages;
 
 namespace Kudashova320
 {
@@ -23,6 +24,7 @@ namespace Kudashova320
         public MainWindow()
         {
             InitializeComponent();
+            navFr.NavigationService.Navigate(new AutorizationPage());
         }
     }
 }
--- Kudashova320/Pages/AutorizationPage.xaml.cs
+++ Kudashova320/Pages/AutorizationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kudashova320.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Kudashova320.Pages
@@ -17,11 +19,34 @@ namespace Kudashova320.Pages
     /// <summary>
     /// Логика взаимодействия для AutorizationPage.xaml
     /// </summary>
-    public partial class AutorizationPage : Window
+    public partial class AutorizationPage : Page
     {
+
+        public List <Human> humans { get; set; }
         public AutorizationPage()
         {
             InitializeComponent();
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTB.Text.Trim();
+            string password = PasswordTB.Password.Trim();
+
+            humans = new List
+            <Human>(DBConnection.catDogEntities.Human.ToList());
+            
+
+            if (login == "1234")
+                if (password == "12345")
+                NavigationService.Navigate(new RaListPage());
+                else
+                    MessageBox.Show(" Введите данные корректно!");
+            if (login == "123")
+                if (password == "1234")
+                    NavigationService.Navigate(new NubiListPage());
+            else 
+             MessageBox.Show(" Введите данные корректно!");
         }
     }
 }
