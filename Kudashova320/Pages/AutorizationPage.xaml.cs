556f4dc6f51b37d9e455e068c0e9b41f0f01f3a2
Анна Кудашова <annakudashova0902@gmail.com>
2023-10-26 08:41:34 +0300

    подключила бд.

---
+++ Kudashova320/Pages/AutorizationPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kudashova320.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationPage.xaml
+    /// </summary>
+    public partial class AutorizationPage : Window
+    {
+        public AutorizationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
